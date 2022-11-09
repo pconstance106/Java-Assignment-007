f16530cd7e73f2d68180679f6bf71e29ced0ad68
netlab-student <pconstance106@mycr.redwoods.edu>
2022-11-09 12:15:20 -0800

    Fall-2022

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
-
 public class MyMath {
-
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("a: ");
+        int a = s.nextInt();
+        System.out.print("b: ");
+        int b = s.nextInt();
+        System.out.println("Greatest common factor: " + greatestCommonFactor(a, b));
+    }
+    public static int greatestCommonFactor(int a, int b) {
+        while (b != 0) {
+            if (a > b) {
+                int c = b;
+                b = a % b;
+                a = c;
+            } else {
+                b = b % a;
+            }
+        }
+        return a;
     }
 }
